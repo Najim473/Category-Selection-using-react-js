d992183446e1a9a60832fc06838492411593a779
Najim Uddin <najim7304@gmail.com>
2022-11-01 20:36:37 +0600

    categories Selection done

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,18 +1,38 @@
-import { useState } from 'react';
-import Products from './Products.js';
-import './Category.css'
+import { useState } from "react";
+import Products from "./Products.js";
+import "./Category.css";
 const Category = () => {
     const [data, setData] = useState(Products);
-    console.log(data);
+    const filterResult = (catItem) => {
+        const result = Products.filter((curData) => {
+            return curData.category === catItem;
+        });
+        setData(result);
+    };
     return (
         <>
-            <h1 className='text-center text-info'>Let's Shop</h1>
+            <h1 className="text-center text-info">Let's Shop</h1>
             <div className="container-fluid mx-2">
                 <div className="row mt-5 mx-2">
                     <div className="col-md-3">
-                        <div className="btn btn-warning w-100 mb-4">Men</div>
-                        <div className="btn btn-warning w-100 mb-4">Women</div>
-                        <div className="btn btn-warning w-100 mb-4">All</div>
+                        <div
+                            className="btn btn-warning w-100 mb-4"
+                            onClick={() => filterResult("Men")}
+                        >
+                            Men
+                        </div>
+                        <div
+                            className="btn btn-warning w-100 mb-4"
+                            onClick={() => filterResult("Women")}
+                        >
+                            Women
+                        </div>
+                        <div
+                            className="btn btn-warning w-100 mb-4"
+                            onClick={() => setData(Products)}
+                        >
+                            All
+                        </div>
                     </div>
                     <div className="col-md-9">
                         <div className="row">
@@ -25,21 +45,23 @@ const Category = () => {
                                                 <img src={img} className="card-img-top" alt="Shirt" />
                                                 <div className="card-body">
                                                     <h5 className="card-title">{title}</h5>
-                                                    <p className='text-primary'>Price {price} TK</p>
+                                                    <p className="text-primary">Price {price} TK</p>
                                                     <p className="card-text">{details}</p>
-                                                    <button href="#" className="btn btn-dark">Buy Now</button>
+                                                    <button href="#" className="btn btn-dark">
+                                                        Buy Now
+                                                    </button>
                                                 </div>
                                             </div>
                                         </div>
                                     </>
-                                )
+                                );
                             })}
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Category
+export default Category;
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -3,6 +3,7 @@ const Products = [
         id: 1,
         title: "Shirt",
         img: "/Images/Shirt-01.jpg",
+        category: "Women",
         price: 2000,
         detils:
             "Some quick example text to build on the card title and make up the bulk of the card",
@@ -11,6 +12,7 @@ const Products = [
         id: 2,
         title: "T-Shirt",
         img: "/Images/T-Shirt-01.jpg",
+        category: "Men",
         price: 1000,
         detils:
             "Some quick example text to build on the card title and make up the bulk of the card",
@@ -19,6 +21,7 @@ const Products = [
         id: 3,
         title: "Shirt",
         img: "/Images/Shirt-02.jpg",
+        category: "Men",
         price: 1800,
         detils:
             "Some quick example text to build on the card title and make up the bulk of the card",
@@ -27,6 +30,7 @@ const Products = [
         id: 4,
         title: "T-Shirt",
         img: "/Images/T-Shirt-02.jpg",
+        category: "Women",
         price: 1500,
         detils:
             "Some quick example text to build on the card title and make up the bulk of the card",
@@ -35,6 +39,7 @@ const Products = [
         id: 5,
         title: "Shirt",
         img: "/Images/Shirt-03.jpg",
+        category: "Men",
         price: 1200,
         detils:
             "Some quick example text to build on the card title and make up the bulk of the card",
@@ -43,6 +48,7 @@ const Products = [
         id: 6,
         title: "T-Shirt",
         img: "/Images/T-Shirt-03.jpg",
+        category: "Women",
         price: 1600,
         detils:
             "Some quick example text to build on the card title and make up the bulk of the card",
